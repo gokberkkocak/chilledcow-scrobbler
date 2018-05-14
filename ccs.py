0c37e808b6e3468ddf7fdff764d5f471cb775772
Gökberk KOÇAK <gk34@st-andrews.ac.uk>
2018-05-14 19:28:28 +0100

    Init code

---
+++ ccs.py
@@ -0,0 +1,93 @@
+import cv2
+import pafy
+import numpy as np
+import time
+from PIL import Image
+import pytesseract
+import pylast
+import difflib
+import sys
+import json
+
+def main():
+    if len(sys.argv) < 3:
+        print("Usage: python ccs.py <credentials> <chilledcow_youtube_stream_link>")
+        sys.exit()
+    credentials_file = sys.argv[1]
+    with open(credentials_file, "r") as f:
+        credentials = json.load(f)
+    url = sys.argv[2]
+    stream = get_video_url(url)
+    prev_song_details = ""
+    while True:
+        image_file = take_snapshot(stream)
+        cropped_image_file = cut_image(image_file)
+        processed_image_file = cv2_process(cropped_image_file)
+        song_details = tesseract_ocr_read(processed_image_file)
+        flag, artist, song, prev_song_details = check_song_details(prev_song_details, song_details)
+        if flag:
+            scrobble_to_lastfm(credentials, artist, song)
+        time.sleep(30)
+        
+def get_video_url(youtube_link):
+    videoPafy = pafy.new(youtube_link)
+    # print(videoPafy)
+    best = videoPafy.getbest()
+    # print(best)
+    stream = best.url
+    # print("Stream link: {}".format(stream))
+    return stream
+
+def take_snapshot(stream):
+    capture = cv2.VideoCapture(stream)
+    success, image = capture.read()
+    image_file = 'img.jpg'
+    cv2.imwrite(image_file, image)
+    capture.release()
+    # print("Snapshot taken")
+    return image_file
+
+def cut_image(image):
+    img = Image.open(image)
+    area = (0, 0, 1000, 50)
+    cropped_img = img.crop(area)
+    new_image = image.split(".")[0]+"_cropped.jpg"
+    cropped_img.save(new_image)
+    # print("Snapshot cropped")
+    return new_image
+
+def cv2_process(image):
+    img = cv2.imread(image)
+    low = np.array([200,200,200])
+    up = np.array([255,255,255])
+    mask = cv2.inRange(img, low, up)
+    res = cv2.bitwise_and(img,img, mask= mask)
+    new_image = image.split(".")[0]+"_processed.jpg"
+    cv2.imwrite(new_image, res)
+    # print("Snapshot background removed")
+    return new_image
+
+def tesseract_ocr_read(image):
+    img = Image.open(image)
+    song_details = pytesseract.image_to_string(img)
+    # print("Song details read by tesseract")
+    return song_details
+
+def check_song_details(previous_song_details, song_details):
+    artist, song = song_details.split(" - ")
+    # print("Current song: {} - {}".format(artist, song))
+    seq = difflib.SequenceMatcher(a=previous_song_details.lower(), b=song_details.lower())
+    if seq.ratio() < 0.95:
+        return True, artist, song, song_details
+    else:
+        return False, None, None, previous_song_details
+
+def scrobble_to_lastfm(credentials, artist, song):
+    lastfm_network = pylast.LastFMNetwork(api_key=credentials["LAST_API_KEY"], api_secret=credentials["LAST_SHARED_SECRET"], \
+                     username= credentials["username"], password_hash=credentials["password_hash"])
+    timestamp = int(time.time())
+    lastfm_network.scrobble(artist, song, timestamp)
+    print("Scrobble completed: {} - {}".format(artist, song))
+
+if __name__ == '__main__':
+    main()
